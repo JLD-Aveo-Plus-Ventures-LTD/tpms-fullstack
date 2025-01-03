cfb41df56e81be349dc1d048e6a4c5e623e534cd
Dare Emmanuel <dareolumuyiwa@jldaveoplus.com>
2025-01-03 00:16:17 +0000

    Update knexfiole and package.json to reflect new changes

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,23 +1,20 @@
-// Update with your config settings.
+require("dotenv").config(); // Load environment variables from .env
 
-/**
- * @type { Object.<string, import("knex").Knex.Config> }
- */
-// knexfile.js
 module.exports = {
   development: {
     client: "mysql2",
     connection: {
-      host: "127.0.0.1",
-      user: "root",
-      password: "",
-      database: "ticket_payment_db",
+      host: process.env.DB_HOST, // Use DB_HOST from .env
+      user: process.env.DB_USER, // Use DB_USER from .env
+      password: process.env.DB_PASSWORD, // Use DB_PASSWORD from .env
+      database: process.env.DB_NAME, // Use DB_NAME from .env
+      port: process.env.DB_PORT, // Use DB_PORT from .env
     },
     migrations: {
-      directory: "./migrations",
+      directory: "./migrations", // Directory for migration files
     },
     seeds: {
-      directory: "./seeds",
+      directory: "./seeds", // Directory for seed files
     },
   },
 };
